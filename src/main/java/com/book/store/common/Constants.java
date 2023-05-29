fcdfbe514c4756398f8310af8652848e396f74c4
pkbagchi <parthabagchi249@gmail.com>
2023-05-29 20:10:49 +0600

    first commit

---
+++ src/main/java/com/book/store/common/Constants.java
@@ -0,0 +1,11 @@
+package com.book.store.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constants {
+
+	public static final String ROLE_ADMIN = "Admin";
+	public static final String MEMBER_OTHER = "Other";
+
+}
